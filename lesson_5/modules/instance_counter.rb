98a12841e85bea23f252e8007521b56127cb8ea2
Nekitoni4 <golubeff.nekit@yandex.ru>
2021-07-29 16:13:54 +0300

    Lesson_5_minor_refactor

--- lesson_5/modules/instance_counter.rb
+++ lesson_5/modules/instance_counter.rb
@@ -3,15 +3,20 @@ module InstanceCounter
   def self.included(klass)
     klass.extend ClassMethods
     klass.include InstanceMethods
-    class << klass
-      attr_accessor :count
-    end
-    klass.count = 0
   end
 
   module ClassMethods
+
+    # Думаю это верное решение, сначала не знал как передать ссылку на
+    # класс в модуль, поэтому иниициализировал в self.included
+    def self.extended(klass)
+      class << klass
+        attr_accessor :count
+      end
+    end
+
     def instances
-      @count
+      @count ||= 0 # Забыл про эту фичу - как в JS ;)
     end
   end
 
